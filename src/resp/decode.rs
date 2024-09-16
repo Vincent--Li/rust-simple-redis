7c4c72c8f8b330023daf18b521d726f9fd8035c7
Vincent_Li <q315744068@gmail.com>
2024-09-16 23:18:23 +0800

    extract common logic

--- src/resp/decode.rs
+++ src/resp/decode.rs
@@ -1,6 +1,6 @@
-use super::{RespDecode, RespError, RespFrame, SimpleString};
+use super::{RespDecode, RespError, RespFrame, SimpleError, SimpleString};
 use anyhow::Result;
-use bytes::BytesMut;
+use bytes::{Buf, BytesMut};
 
 impl RespDecode for RespFrame {
     fn decode(buf: &mut BytesMut) -> Result<Self, RespError> {
@@ -16,29 +16,7 @@ impl RespDecode for RespFrame {
 
 impl RespDecode for SimpleString {
     fn decode(buf: &mut BytesMut) -> Result<Self, RespError> {
-        if buf.len() < 3 {
-            return Err(RespError::NotComplete);
-        }
-
-        if !buf.starts_with(b"+") {
-            return Err(RespError::InvalidFrameType(format!(
-                "expected SimpleString(+)' but got {:?}",
-                buf
-            )));
-        }
-
-        // search for \r\n
-        let mut end = 0;
-        for i in 0..buf.len() - 1 {
-            if buf[i] == b'\r' && buf[i + 1] == b'\n' {
-                end = i;
-                break;
-            }
-        }
-
-        if end == 0 {
-            return Err(RespError::NotComplete);
-        }
+        let end = extract_simple_frame_data(buf, "+")?;
 
         // split the buffer
         let data = buf.split_to(end + 2);
@@ -46,6 +24,68 @@ impl RespDecode for SimpleString {
 
         Ok(SimpleString::new(s.to_string()))
     }
+}
+
+impl RespDecode for SimpleError {
+    fn decode(buf: &mut BytesMut) -> Result<Self, RespError> {
+        let end = extract_simple_frame_data(buf, "-")?;
+
+        let data = buf.split_to(end + 2);
+        let s = String::from_utf8_lossy(&data[1..end]);
+
+        Ok(SimpleError::new(s.to_string()))
+    }
+}
+
+#[allow(dead_code)]
+fn extract_fixed_data(
+    buf: &mut BytesMut,
+    expect: &str,
+    expect_type: &str,
+) -> Result<(), RespError> {
+    if buf.len() < expect.len() {
+        return Err(RespError::NotComplete);
+    }
+
+    if !buf.starts_with(expect.as_bytes()) {
+        return Err(RespError::InvalidFrameType(format!(
+            "expect: {}, got: {:?}",
+            expect_type, buf
+        )));
+    }
+
+    buf.advance(expect.len());
+    Ok(())
+}
+
+fn extract_simple_frame_data(buf: &[u8], prefix: &str) -> Result<usize, RespError> {
+    if buf.len() < 3 {
+        return Err(RespError::NotComplete);
+    }
+
+    if !buf.starts_with(prefix.as_bytes()) {
+        return Err(RespError::InvalidFrameType(format!(
+            "expected SimpleString(+)' but got {:?}",
+            buf
+        )));
+    }
+
+    let end = find_crlf(buf, 1).ok_or(RespError::NotComplete)?;
+    Ok(end)
+}
+
+fn find_crlf(buf: &[u8], nth: usize) -> Option<usize> {
+    let mut count = 0;
+    for i in 1..buf.len() - 1 {
+        if buf[i] == b'\r' && buf[i + 1] == b'\n' {
+            count += 1;
+            if count == nth {
+                return Some(i);
+            }
+        }
+    }
+
+    None
 }
 
 #[cfg(test)]
@@ -68,6 +108,23 @@ mod tests {
         let frame = SimpleString::decode(&mut buf)?;
         assert_eq!(frame, SimpleString::new("hello".to_string()));
 
+        Ok(())
+    }
+
+    #[test]
+    fn test_simple_error_decode() -> Result<()> {
+        let mut buf = BytesMut::new();
+        buf.extend_from_slice(b"-ERR unknown command 'hello'\r\n");
+
+        let frame = SimpleError::decode(&mut buf)?;
+        assert_eq!(
+            frame,
+            SimpleError::new("ERR unknown command 'hello'".to_string())
+        );
+
+        buf.extend_from_slice(b"-ERR unknown command 'hello'");
+        let ret = SimpleError::decode(&mut buf);
+        assert_eq!(ret, Err(RespError::NotComplete));
         Ok(())
     }
 }
